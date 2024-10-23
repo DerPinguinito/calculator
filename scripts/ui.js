a2f7513b68d50ca87502b0f64bfc4cc0e7c87eb7
Herr Pinguinito <176357385+DerPinguinito@users.noreply.github.com>
2024-10-23 13:50:41 -0300

    Adds basic functionality to buttons
    All buttons now print to the calculator screen when clicked. Also,
    the updateValue function of MyUI prevents a value from starting with 00.

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,16 +1,42 @@
 import calc from "./calculator.js"
 
 export default class MyUI {
-    static #updateSCREEN() {
+    static #updateScreen() {
         const OUTPUT = document.getElementById("output")
         if (OUTPUT) {
             console.log("Output element found")
-            OUTPUT.textContent = calc.output
-            OUTPUT.textContent ? OUTPUT.textContent = null : OUTPUT.textContent = 0
+            OUTPUT.textContent ? null : calc.output = '0'
+            OUTPUT.textContent = calc.output;
+        }
+    }
+    static #updateValue() {
+        if (calc.output[0] === '0' && calc.output[1] != '.') {
+            calc.output = calc.output.substring(1)
+        }
+        if (calc.output[0] === '0' && calc.output[1] === '0') {
+            calc.output = '0'
+        }
+        
+    }
+    static #setEventToNumericButton(button) {
+        button.addEventListener('click', (e) => {
+            calc.output += e.target.textContent
+            MyUI.#updateValue()
+            MyUI.#updateScreen()
+        })
+    }
+    static #sortButtonEvents(button) {
+        switch(true) {
+            default:
+                MyUI.#setEventToNumericButton(button)
+                break
         }
     }
     static #main() {
-        MyUI.#updateSCREEN()
+        MyUI.#updateScreen()
+        const BUTTONS = document.body.querySelectorAll("button")
+        BUTTONS.forEach(button => MyUI.#sortButtonEvents(button))
+        
     }
 
     #initiate = MyUI.#main()
